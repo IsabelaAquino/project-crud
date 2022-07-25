dd80dfd20673d24eee8155d69ce54d6ff716e4d1
isabeladommus <isabela.aquino@dommus.com.br>
2022-07-25 20:49:01 -0300

    melhorias, metodo de edit e add readme

--- src/views/Produtos.vue
+++ src/views/Produtos.vue
@@ -4,6 +4,43 @@
     fluid
     tag="section"
   >
+    <v-dialog
+      v-model="dialogRemover"
+      max-width="300"
+    >
+      <v-card>
+        <v-card-title>
+          Deseja remover esse produto?
+
+          <v-spacer />
+
+          <v-icon
+            aria-label="Close"
+            @click="dialogRemover = false"
+          >
+            mdi-close
+          </v-icon>
+        </v-card-title>
+
+        <v-card-text class="pb-6 pt-12 text-center">
+          <v-btn
+            class="mr-3"
+            text
+            @click="dialogRemover = false"
+          >
+            Não
+          </v-btn>
+
+          <v-btn
+            color="success"
+            text
+            @click="dialogRemover = false; removerItem()"
+          >
+            Sim
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
     <material-alert
       v-bind:color="returnRequest"
       dark
@@ -27,6 +64,7 @@
               color="indigo"
               style="margin-left: 94% !important;"
               v-bind="attrs"
+              @click.prevent="veioEditar = false, produto = {}"
               v-on="on"
             >
               <v-icon dark>
@@ -37,11 +75,14 @@
 
           <v-card>
             <v-card-title class="text-h5 grey lighten-2">
-              Adicionar Produto
+              {{
+                !veioEditar ?
+                'Adicionar Produto' : 'Editar Produto'
+              }}
             </v-card-title>
 
             <v-card-text>
-              <ProdutoAdicionar @atualizaProdutos="atualizaProdutos" />
+              <ProdutoAdicionar :produto="produto" @atualizaProdutos="atualizaProdutos" />
             </v-card-text>
 
             <v-divider></v-divider>
@@ -88,12 +129,12 @@
                 {{produto.price}}
               </td>
               <td>
-                <v-icon color="#00aeff" dark>
+                <v-icon @click.prevent="editButton(produto)" color="#00aeff" dark>
                   mdi-auto-fix
                 </v-icon>
               </td>
               <td>
-                <v-icon @click.prevent="removerItem(produto)" color="#FF0000" dark>
+                <v-icon @click.prevent="trashButton(produto)" color="#FF0000" dark>
                   mdi-delete
                 </v-icon>
               </td>
@@ -127,12 +168,15 @@
     data() {
       return {
         produtos: null,
+        produto: {},
         produtosPorPagina: 10,
         produtosTotal: 20,
         modalAddProduto: false,
         notificacao: false,
         returnRequest: "success",
-        returnRequestMessagem: ""
+        returnRequestMessagem: "",
+        dialogRemover: false,
+        veioEditar: false
       };
     },
     computed: {
@@ -148,7 +192,6 @@
       getProdutos() {
         this.produtos = null;
         ProdutosService.get(this.url).then(response => {
-          console.log("res", response)
           this.produtosTotal = Number(response.headers["x-total-count"]);
           this.produtos = response.data;
         });
@@ -162,6 +205,20 @@
           this.returnRequestMessagem = 'Produto Adicionado com Sucesso.'
         else
           this.returnRequestMessagem = 'Ocorreu um erro ao adicionar um novo produto.'
+
+        //aqui faria o update da nova lista de produtos se a api fosse com dados reais
+        this.getProdutos()
+      },
+
+      trashButton(produto){
+        this.dialogRemover = true; 
+        this.produto = produto
+      },
+
+      editButton(produto){
+        this.veioEditar = true
+        this.modalAddProduto = true
+        this.produto = produto
       },
 
       ativaNotificacao(retorno){
@@ -173,17 +230,19 @@
         }, 1500);
       },
 
-      removerItem(produto){
-        if(confirm('Deseja excluir esse produto?')){
-          ProdutosService.delete(produto.id).then((response) => {
-            this.ativaNotificacao("success")
-            this.returnRequestMessagem = 'Produto removido com sucesso!'
-          }
-          ).catch(error =>{
-            this.ativaNotificacao("error")
-            this.returnRequestMessagem = 'Ocorreu um erro ao remover esse produto.'
-          })
+      removerItem(){
+
+        ProdutosService.delete(this.produto.id).then((response) => {
+          this.ativaNotificacao("success")
+          this.returnRequestMessagem = 'Produto removido com sucesso!'
+          //aqui faria o update da nova lista de produtos se a api fosse com dados reais
+          this.getProdutos()
         }
+        ).catch(error =>{
+          this.ativaNotificacao("error")
+          this.returnRequestMessagem = 'Ocorreu um erro ao remover esse produto.'
+        })
+      
       }
     },
     watch: {
